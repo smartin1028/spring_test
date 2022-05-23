fdff63bdbc52830c4a3dbed788cecef9a0f6b528
daewon <leedw3232@gmail.com>
2022-05-23 21:58:53 +0900

    팩토리 리팩토링

--- src/main/java/com/spring_test/dao/DaoFactory.java
+++ src/main/java/com/spring_test/dao/DaoFactory.java
@@ -2,13 +2,18 @@ package com.spring_test.dao;
 
 public class DaoFactory {
 	public UserDao userDao(){
-		return new UserDao(new DConnectionMaker());
+		return new UserDao(connectionMaker());
 	}
+
+	private DConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
+
 	public AccountDao accountDao(){
-		return new AccountDao(new DConnectionMaker());
+		return new AccountDao(connectionMaker());
 	}
 	public MessageDao messageDao(){
-		return new MessageDao(new DConnectionMaker());
+		return new MessageDao(connectionMaker());
 	}
 
 
