747e14249c7e70739e918238a0fa82cb36bc7ba9
daewon <leedw3232@gmail.com>
2022-05-23 20:01:35 +0900

    first commit

---
+++ src/main/java/com/spring_test/dao/UserDao.java
@@ -0,0 +1,48 @@
+package com.spring_test.dao;
+
+import com.spring_test.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:h2:tcp://localhost/~/spring", "sa", ""
+		);
+
+		PreparedStatement ps = c.prepareStatement(
+				"insert into tb_user(id, name,password) values (?,?,?)"
+		);
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		ps.executeUpdate();
+		ps.close();
+		c.close();
+
+	}
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:h2:tcp://localhost/~/spring", "sa", ""
+		);
+
+		PreparedStatement ps = c.prepareStatement(
+				"select * from tb_user where id = ?"
+		);
+		ps.setString(1, id);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+
+		rs.close();
+		ps.close();
+		c.close();
+		return user;
+	}
+}
---
+++ src/main/java/com/spring_test/domain/User.java
@@ -0,0 +1,34 @@
+package com.spring_test.domain;
+
+import lombok.Data;
+
+
+public class User {
+	String id;
+	String name;
+	String password;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
