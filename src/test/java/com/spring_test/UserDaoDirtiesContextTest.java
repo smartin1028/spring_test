ccd92a07afb93a45653b2e166ed3d18b5a9e44e6
daewon <leedw3232@gmail.com>
2022-05-25 21:58:08 +0900

    DirtiesContext

--- src/test/java/com/spring_test/UserDaoDirtiesContextTest.java
+++ src/test/java/com/spring_test/UserDaoDirtiesContextTest.java
@@ -1,6 +1,7 @@
 package com.spring_test;
 
-import com.spring_test.dao.*;
+import com.spring_test.dao.UserDao;
+import com.spring_test.domain.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,29 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@ContextConfiguration(locations = "/config/spring/ApplicationContext.xml")
-class UserDaoAnnotationTest {
+@DirtiesContext
+class UserDaoDirtiesContextTest {
 
-	@Autowired
-	ApplicationContext context;
-
-	@Autowired SimpleDriverDataSource dataSource;
-	@Autowired DataSource dataSource3;
-
-	@Autowired
 	UserDao dao;
 
 	@BeforeEach
 	public void beforeEach(){
-		System.out.println("dataSource = " + dataSource3);
-		System.out.println("dataSource = " + (dataSource == dataSource3));
+		DataSource dataSource = new SingleConnectionDataSource(
+				"jdbc:h2:tcp://localhost/~/spring", "sa" , "", true
+		);
+		dao = new UserDao();
+		dao.setDataSource(dataSource);
 	}
 
 	@DisplayName("컨텍스트 테스트")
@@ -38,6 +36,9 @@ class UserDaoAnnotationTest {
 	public void contestTest() throws Exception {
 		Integer count = dao.count();
 		assertThat(count).isNotNull();
+		dao.add(new User("id", "name" , "pass"));
+		assertThat(dao.count()).isEqualTo(count + 1);
+
 	}
 
 
